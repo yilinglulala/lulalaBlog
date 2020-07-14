da8a6f66b048dd88534ef62647fa0964ac20fd48
yilinglulala <31814122+yilinglulala@users.noreply.github.com>
2020-07-14 17:44:53 +0800

    修改图片路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: '鹿啦啦',
     description: '鹿啦啦的博客',
     //  指定 vuepress build 的输出目录
-    dest: './dist',
+    dest: 'F:/test/lulalaBlog-template/vuepressBlog',
     // 注入到当前页面的 HTML <head> 中的标签
     head: [
         ['link', { rel: 'icon', href: '/img/logo.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
